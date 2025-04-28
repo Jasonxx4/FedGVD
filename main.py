a8d1e5a6e962927560c9bd166086c0fc520f62ac
Jasonxx4 <145441287+Jasonxx4@users.noreply.github.com>
2025-04-28 20:49:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import openfgl.config as config
+
+
+from openfgl.flcore.trainer import FGLTrainer
+
+args = config.args
+
+args.root = "dataset"
+
+
+args.dataset = ["Computers"]
+args.simulation_mode = "subgraph_fl_louvain"
+args.num_clients = 10
+
+
+# if True:
+#     args.fl_algorithm = "fedavg"
+#     args.model = ["gcn"]
+# else:
+args.fl_algorithm = "fedgvd"
+# args.model = ["gcn", "gat", "sgc", "mlp", "graphsage"] # choose multiple gnn models for model heterogeneity setting.
+args.model = ["gcn","gat", "sgc","graphsage"] # choose multiple gnn models for model heterogeneity setting.
+# args.model = ["gcn","gat"] # choose multiple gnn models for model heterogeneity setting.
+# args.model = ["gcn"] # choose multiple gnn models for model heterogeneity setting.
+
+args.metrics = ["accuracy"]
+
+
+# 初始化客户端，分配数据集
+trainer = FGLTrainer(args)
+
+trainer.train()
